c045568cce2cc2dd2e3690dab0c1370f4df0ed7c
craftsmon <craftsmon@outlook.com>
2017-06-04 13:43:56 +0300

    Added some functionality on posting a job vacancy

--- app/home/forms.py
+++ app/home/forms.py
@@ -1,6 +1,28 @@
 from flask_wtf import FlaskForm
+from wtforms import FieldList, FormField, PasswordField, StringField, SubmitField, TextAreaField, ValidationError
+from wtforms.validators import DataRequired, Email, EqualTo
 
+"""
+Miniature forms used in FormField
+"""
+class RolesForm(FlaskForm):
+    role = StringField('Role Description', validators = [DataRequired()])
+
+
+"""
+Forms that will get rendered
+"""
 class ApplicationForm(FlaskForm):
     """
     Allows users to apply for a position
     """
+
+class JobPostForm(FlaskForm):
+    """
+    Allows user to post a job vacancy
+    """
+    company = StringField("Company Name", validators = [DataRequired()])
+    position = StringField('Position', validators = [DataRequired()])
+    email = StringField('Email Contacts', validators = [DataRequired(), Email()])
+    description = TextAreaField('Job Description', validators = [DataRequired()])
+    submit = SubmitField('Submit')
--- app/home/views.py
+++ app/home/views.py
@@ -1,14 +1,47 @@
-from flask import render_template
+from flask import flash, redirect, render_template, url_for
 from flask_login import current_user, login_required
 
 from . import home
+from .forms import JobPostForm
+from .. import db
+from ..models import Job
 
 @home.route('/')
 @login_required
 def index():
     return render_template('home/index.html')
 
+@home.route('/apply/<int:id>',methods=['GET', 'POST'])
+@login_required
+def apply(id):
+    """
+    Apply for selected position
+    """
+    job = Job.query.get_or_404(id)
+
+
+    return render_template('home/apply.html', job=job)
+
 @home.route('/jobs')
 @login_required
 def jobs():
-    return render_template('home/jobs.html')
+    # get all rows
+    jobs = Job.query.all()
+
+    return render_template('home/jobs.html', jobs=jobs)
+
+@home.route('/post', methods = ['GET', 'POST'])
+def post():
+    form = JobPostForm()
+    if form.validate_on_submit():
+        post = Job(description=form.description.data, position=form.position.data, email=form.email.data)
+
+        # add new vacancy to database
+        db.session.add(post)
+        db.session.commit()
+        flash("You have successfully added a job vacancy post.")
+
+        #redirect to jobs page
+        return redirect(url_for("home.jobs"))
+
+    return render_template('home/post.html', form=form)
