c7b2791358c1ac040b1001957d61b25bc93af4b9
craftsmon <craftsmon@outlook.com>
2017-06-09 08:23:46 +0300

    Remove from hidden input objects when remove button is clicked on html also refresh input value after skill is added

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,7 +1,7 @@
 $('onready', function(){
   $('form.form div.radio').addClass('radio-inline');
 
-  var $skill_skill_hidden_input = $('input[name=skill]');
+  var $skill_hidden_input = $('input[name=skill]');
 
   var skillset = [];
 
@@ -11,21 +11,21 @@ $('onready', function(){
     // check for value in input[user-skill]
     if (skill) {
 
+      // skillset.push({skill});
       skillset.push({skill});
 
       // console.log(JSON.stringify(skillset));
 
-      $skill_skill_hidden_input.val(JSON.stringify(skillset));
 
       var table_row_count = $("table tbody tr").length;
       var table_row_id = table_row_count +1;
 
-      var markup_no = "<td>"+ table_row_id +"</td>";
-      var markup_skill = "<td>"+ skill +"</td>";
+      // var markup_no = "<td>"+ table_row_id +"</td>";
+      var markup_skill = '<td id="skill">'+ skill +'</td>';
       var markup_remove = '<td><button id="btn-remove-row" type="button" class="btn btn-link"> Remove </button></td>';
 
       var markup_row = '<tr id="table-row-'+table_row_id+'">' +
-                        markup_no +
+                        // markup_no +
                         markup_skill +
                         markup_remove +
                         '</tr>';
@@ -33,12 +33,21 @@ $('onready', function(){
       // add a row with skill and
       // experience level onto the table
       $("table tbody").prepend(markup_row);
+
     }
+    // clear input
+    $("input[name=user-skill]").val("");
 
     // Find and remove selected table rows
     $("button#btn-remove-row").click(function() {
+      var to_remove = $(this).parents("td").siblings("td#skill").text();
+      skillset = skillset.filter(function(el) {
+        return el.skill !== to_remove;
+      });
       $(this).parents("tr").remove();
+      $skill_hidden_input.val(JSON.stringify(skillset));
     });
 
+    $skill_hidden_input.val(JSON.stringify(skillset));
   });
 });
